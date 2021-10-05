3b778975b66d4c8a7dbcb11a8a0fb2d9af41286e
LisandroPer <lisanperri95@hotmail.com>
2021-10-05 15:00:21 -0300

    Prueba con cards

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,4 @@
+//----------------------------------------------------FUNCIONES PARA USUARIOS-----------------------------------------------------------//
 function registrarUsuarios(){
     //Obtención del mail/nombre del usuario
     let ingresoNombreUsuario = document.getElementById("exampleInputEmail1");
@@ -34,42 +35,67 @@ function registrarUsuarios(){
     }
 }
 
+//------------------------------------------------------------FUNBCIONES PARA ALFAJORES----------------------------------------------------------//
 
+function alfajoresUI(alfajores, idAlfajor){
+    for(const alfajor of alfajores){
+        //forma de crear el div con los objetos alfajor a través de JQuerry.
+        $("#contenedorDeProductos").append(`<div class="card text-white bg-dark" style="width: 18rem;">
+                                             <img src="/Imagenes/alfajorbali.jpg" class="card-img-top" alt="...">
+                                             <div class="card-body">
+                                                 <h5 class="card-title">${alfajor.nombreAlfajor}</h5>
+                                                 <p class="card-text">Precio: ${alfajor.precio}</p>
+                                                 <button id="${alfajor.idAlfajor}" class = "btn btn-primary">Buy</button>
+                                              </div>
+                                            </div>`);
+        
+        //LA DE ABAJO ES LA FORMA DE HACERLO SIN JQUERRY.
+        //document.getElementById(idAlfajor).appendChild(creadorDeAlfajoresHTML(alfajor));
+    }
+}
 
+/* FORMA DE CREAR EL DIV CON LOS OBJETOS SIN UTILIZAR JQUERRY.
 function creadorDeAlfajoresHTML(){
     for(const alfajor of alfajores){
         let divAlfajor = document.createElement("div");
         //Crea los elementos div que contendran la información de los alfajores. 
-        divAlfajor.innerHTML = `<h3>${alfajor.nombreAlfajor}</h3>
-                                <h4>"Precio: "${alfajor.precio}</h4>
-                                <button id="${alfajor.idAlfajor}" class = "btnCompra">Buy</button>`
+        divAlfajor.innerHTML = `<div class="card text-white bg-dark" style="width: 18rem;">
+                                 <img src="/Imagenes/alfajorbali.jpg" class="card-img-top" alt="...">
+                                 <div class="card-body">
+                                    <h5 class="card-title">${alfajor.nombreAlfajor}</h5>
+                                    <p class="card-text">Precio: ${alfajor.precio}</p>
+                                    <button id="${alfajor.idAlfajor}" class = "btn btn-primary">Buy</button>
+                                 </div>
+                                </div>`
         
                                 //Agrega el hijo, en este caso el div, al cuerpo del HTML.
-                                document.body.appendChild(divAlfajor);
+                                //document.body.appendChild(divAlfajor);
+            return divAlfajor;
     }
 }
+*/
 
 function botonDeComprar(){
     //Obtención de los elementos de la clase btnCompra
-let botones = document.getElementsByClassName("btnCompra");
-//Comprobación de su obtención mediante el console.log.
-console.log(botones);
-for(const boton of botones){
-    //Se escuchará el evento "click" y saldrá por consola el id del alfajor seleccionado.
-   boton.addEventListener("click",function(){
-       let seleccion = alfajores.find(alfajor => alfajor.idAlfajor == this.id);
-       //Saldia por consola del nombre del alfajor seleccionado.
-       console.log("El alfajor seleccionado es: " + seleccion.nombreAlfajor + "y su precio es de: " + seleccion.precio);
-       //Se agrega al arrayd "carritoCompras" el nombre del alfajor seleccionado. 
-       carritoCompras.push(seleccion);
-       //Guardo la información de carrito de compras en el localStorage.
-       localStorage.setItem("alfajoresEnCarrito",JSON.stringify(carritoCompras));
+   let botones = document.getElementsByClassName("btn btn-primary");
+   //Comprobación de su obtención mediante el console.log.
+   console.log(botones);
+   for(const boton of botones){
+      //Se escuchará el evento "click" y saldrá por consola el id del alfajor seleccionado.
+     boton.addEventListener("click",function(){
+         let seleccion = alfajores.find(alfajor => alfajor.idAlfajor == this.id);
+         //Saldia por consola del nombre del alfajor seleccionado.
+         console.log("El alfajor seleccionado es: " + seleccion.nombreAlfajor + "y su precio es de: " + seleccion.precio);
+         //Se agrega al arrayd "carritoCompras" el nombre del alfajor seleccionado. 
+         carritoCompras.push(seleccion);
+         //Guardo la información de carrito de compras en el localStorage.
+         localStorage.setItem("alfajoresEnCarrito",JSON.stringify(carritoCompras));
 
-       let consoleCarrito = JSON.parse(localStorage.getItem("alfajoresEnCarrito"));
-       console.log(consoleCarrito);
-   })
-}
+        let consoleCarrito = JSON.parse(localStorage.getItem("alfajoresEnCarrito"));
+        console.log(consoleCarrito);
+      })
+    }
 
-//Comprobación de los nombres guardados en la arrayd.
-console.log(carritoCompras);
+  //Comprobación de los nombres guardados en la arrayd.
+  console.log(carritoCompras);
 }
--- js/main.js
+++ js/main.js
@@ -13,11 +13,15 @@ alfajores.push(new Alfajor(2,"Alfajor de mantequilla de maní",40));
 alfajores.push(new Alfajor(3,"Alfajor de frutilla",30));
 alfajores.push(new Alfajor(4,"Alfajor de chocolate blanco",35));
 
+alfajoresUI(alfajores, "contenedorDeProductos");
+
 //Comprobación de que los objetos se necuentran dentro de la arrayd.
 console.log(alfajores);
 
 //Llamado al método creador de los objetos alfajor en el HTML.
-creadorDeAlfajoresHTML();
+//creadorDeAlfajoresHTML();
 
 //llamado al método del botón de compra.
 botonDeComprar();
+
+console.log($("#contenedorDeProductos"));
