c2a8371f223fa125c67095f4fac436451c3b26a3
LisandroPer <lisanperri95@hotmail.com>
2021-11-02 18:11:16 -0300

    BOTON ELIMINAR PRODUCTO FUNCIONAL. SOLO HABÍA QUE AGREGARLE UN DIV COMO PADRE...

--- js/funciones.js
+++ js/funciones.js
@@ -34,8 +34,8 @@ function creadorDeAlfajoresHTML(){
                                  </div>
                                 </div>`
         
-                                //Agrega el hijo, en este caso el div, al cuerpo del HTML.
-                                //document.body.appendChild(divAlfajor);
+                            //Agrega el hijo, en este caso el div, al cuerpo del HTML.
+                            document.body.appendChild(divAlfajor);
             return divAlfajor;
     }
 }
@@ -52,10 +52,13 @@ function botonDeComprar(){
      boton.addEventListener("click",function(){
          let seleccion = alfajores.find(alfajor => alfajor.idAlfajor == this.id);
          //Saldia por consola del nombre del alfajor seleccionado.
-         console.log("El alfajor seleccionado es: " + seleccion.nombreAlfajor + "y su precio es de: " + seleccion.precio);
+         
+         //console.log("El alfajor seleccionado es: " + seleccion.nombreAlfajor + "y su precio es de: " + seleccion.precio);
+         
          //Se agrega al arrayd "carritoCompras" el nombre del alfajor seleccionado. 
          carritoCompras.push(seleccion);
          //Guardo la información de carrito de compras en el localStorage.
+        
          localStorage.setItem("alfajoresEnCarrito",JSON.stringify(carritoCompras));
 
         let consoleCarrito = JSON.parse(localStorage.getItem("alfajoresEnCarrito"));
@@ -75,15 +78,26 @@ function botonDeComprar(){
 //Función encargada de añadir los productos seleccionados por el usuario al carrito de comrpas del HTML.
 function carritoUI(carritoCompras){
   //traigo la info del if del html de carritoCantidad. Le agrego la información de la arrayd carritoCompras.
+  //El método html () en jQuery se utiliza para establecer o devolver el contenido innerHTML del elemento seleccionado. 
    $("#carritoCantidad").html(carritoCompras.length);
    //Borro los elementos anteriores para que no se repita la arrayd entera de nombre y que se vean solo los alfajores seleccionados.
    $("#carritoDeProductos").empty();
    
    //Introduzco los nobmres y el precio de los productos seleccionados en el carrito del HTML.
   for (const alfajor of carritoCompras) {
-     const alfajoresHTMLcarrito = `<p>${alfajor.nombreAlfajor} - ${alfajor.precio} $</p> <button id="btnEliminar" class="btnEliminar1 btn btn-danger">Delete purchase</button>`;
+     const alfajoresHTMLcarrito = `<div class="producto-carrito"><p>${alfajor.nombreAlfajor} - ${alfajor.precio} $</p> <button id="eliminar${alfajor.idAlfajor}" class="btnEliminar1 btn btn-danger">Delete purchase</button> <div>`;
      $("#carritoDeProductos").append(alfajoresHTMLcarrito);
        //`<p>${alfajor.nombreAlfajor} - ${alfajor.precio} $</p> <button id="btnEliminar" class="btnEliminar btn btn-danger">Delete purchase</button>`);
+       //ELIMINAR PRODUCTO DEL CARRITO
+       
+       let botonEliminar = document.getElementById(`eliminar${alfajor.idAlfajor}`);
+       console.log(botonEliminar.parentElement);
+
+       botonEliminar.addEventListener("click", function(){
+         carritoCompras = carritoCompras.filter(elemento => elemento.idAlfajor =! alfajoresHTMLcarrito);
+         botonEliminar.parentElement.remove();
+       })
+       
   }
   //agrego el botón cofirmar compra que me permitirá enviar una solicitud al back end.
   $("#carritoDeProductos").append(`<button id="btnConfirmar" class="btn btn-success">Confirm purchase</button>`);
@@ -92,24 +106,9 @@ function carritoUI(carritoCompras){
 }
 
 
-//ELIMINAR ALFAJOR
-function findAlfajorParaEliminar(){
-  for (const botonEliminar of botonesEliminar) {
-    botonEliminar = document.getElementsByClassName("btnEliminar1 btn btn-danger");
-    
-    botonEliminar.addEventListener("click",function(){
-     
-      carritoCompras.find(alfajor => alfajor.idAlfajor == this.id);
-      alfajor.remove();
-    })
-    }
-}
-function eliminarElProducto(){
-  $(".btnEliminar1 btn btn-danger").on("click",function(){
-    findAlfajorParaEliminar();
-    console("va bien");
-  })
-}
+
+
+
 
 
 
@@ -152,13 +151,19 @@ function registrarUsuarios(){
     //guardo la contraseña y el nombre de usuario en una variable
     let nombreUsuario = $("#exampleInputEmail1").val();
     let contrasenaUsuario = $("#exampleInputPassword1").val();
+
+    //El siguiente if es para que el usuario no deje los inputs en blanco.
+    if(nombreUsuario == "" || contrasenaUsuario == ""){
+       console.log("Lo siento, debe completar ambos campos para continuar")
+    }else{
+      console.log("Datos completados...")
+       //pusheo los datos de las variables a la Arrayd de usuarios.
+       usuarios.push(new Usuario(nombreUsuario,contrasenaUsuario));
+       console.log(usuarios);
     
-    //pusheo los datos de las variables a la Arrayd de usuarios.
-    usuarios.push(new Usuario(nombreUsuario,contrasenaUsuario));
-    console.log(usuarios);
-    
-    //llamo al método para subir los datos al servidor.
-    enviarInformacionDeUsuario();
+       //llamo al método para subir los datos al servidor.
+       enviarInformacionDeUsuario();
+    }
    })
 }
 
